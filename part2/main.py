87957dca5778d978aeb2ce1ed61f92dbb6a1fcb3
ArrowGreen1 <asinible@gmail.com>
2022-11-13 20:36:08 +0600

    part2

---
+++ part2/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, APIRouter
+
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
+
+app = FastAPI(title='Recipe API', openapi_url='/openapi.json')
+
+api_router = APIRouter()
+
+
+@api_router.get('/', status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    :return: dict
+    """
+    return {'msg': 'Hello World!'}
