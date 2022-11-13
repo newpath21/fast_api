1cd3ba8271921aacdb0672843e84002961132b75
ArrowGreen1 <asinible@gmail.com>
2022-11-13 21:02:09 +0600

    add run

--- part2/app/main.py
+++ part2/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, APIRouter
 
+from typing import Optional
+
 RECIPES = [
     {
         "id": 1,
@@ -33,3 +35,29 @@ def root() -> dict:
     :return: dict
     """
     return {'msg': 'Hello World!'}
+
+
+@api_router.get('/recipe/{recipe_id}', status_code=200)
+def fetch_recipe(*, recipe_id: int) -> dict:
+    result = [recipe for recipe in RECIPES if recipe['id'] == recipe_id]
+    if result:
+        return result[0]
+
+
+@api_router.get('/search/', status_code=200)
+def search_recipes(
+        keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    if not keyword:
+        return {'results': RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower() in recipe['label'].lower(), RECIPES)
+    return {'results': list(results)[:max_results]}
+
+
+app.include_router(api_router)
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host='0.0.0.0', port=8001, log_level='debug')
