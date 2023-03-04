0745b8dccb4318e1caf2090773dbce3b3a8ff48d
asinible <asinible@gitlab.com>
2023-03-04 23:09:07 +0600

    database configured

--- project/app/main.py
+++ project/app/main.py
@@ -4,64 +4,90 @@ from fastapi.templating import Jinja2Templates
 from typing import Optional, Any
 from pathlib import Path
 
-from app.schemas import RecipeSearchResults
+from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
+from app.recipe_data import RECIPES
 
-RECIPES = [
-    {
-        "id": 1,
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
-    },
-    {
-        "id": 2,
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
-    },
-    {
-        "id": 3,
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
-    },
-]
 
-app = FastAPI(title='Recipe API', openapi_url='/openapi.json')
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
 
-@api_router.get('/', status_code=200)
-def root() -> dict:
+# Updated to serve a Jinja2 template
+# https://www.starlette.io/templates/
+# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
+@api_router.get("/", status_code=200)
+def root(request: Request) -> dict:
     """
     Root GET
-    :return: dict
     """
-    return {'msg': 'Hello World!'}
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": RECIPES},
+    )
 
 
-@api_router.get('/recipe/{recipe_id}', status_code=200)
-def fetch_recipe(*, recipe_id: int) -> dict:
-    result = [recipe for recipe in RECIPES if recipe['id'] == recipe_id]
-    if result:
-        return result[0]
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
+def fetch_recipe(*, recipe_id: int) -> Any:
+    """
+    Fetch a single recipe by ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if not result:
+        # the exception is raised, not returned - you will get a validation
+        # error otherwise.
+        raise HTTPException(
+            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
+        )
+
+    return result[0]
 
 
-@api_router.get('/search/', status_code=200)
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
-        keyword: Optional[str] = None, max_results: Optional[int] = 10
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
+    max_results: Optional[int] = 10,
 ) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
     if not keyword:
-        return {'results': RECIPES[:max_results]}
+        # we use Python list slicing to limit results
+        # based on the max_results query parameter
+        return {"results": RECIPES[:max_results]}
 
-    results = filter(lambda recipe: keyword.lower() in recipe['label'].lower(), RECIPES)
-    return {'results': list(results)[:max_results]}
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
+
+
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+    """
+    Create a new recipe (in memory only)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.dict())
+
+    return recipe_entry
 
 
 app.include_router(api_router)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
     import uvicorn
 
-    uvicorn.run(app, host='0.0.0.0', port=8001, log_level='debug')
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
